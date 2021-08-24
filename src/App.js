967742c97fa6af2681860872062d0e0cf1d38049
Mufolk <guilhermemelo138@gmail.com>
2021-08-24 13:21:42 -0300

    refactoring main app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 
-const App = () => {
-  return <div>Hello World!</div>;
-};
+class App extends Component {
+  render() {
+    return <div>Hello World</div>;
+  }
+}
 
 export default App;
